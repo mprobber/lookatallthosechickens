7fed223aadf82f6d3859dfea574b0702b22ddfd4
Michael Probber <git@mikep.ro>
2020-08-19 18:18:03 -0700

    we did it

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useCallback, useEffect, useRef} from 'react';
+
+import daremix from './daremix.mp3';
 import './App.css';
 
+function getRandomNumber(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function Chicken({lock, top, left, onLoad}) {
+
+  return <img src={`https://loremflickr.com/320/240/chicken?lock=${lock}`} onLoad={onLoad} style={{position: 'absolute', top, left}} alt="chicken" />
+}
+
 function App() {
+  const [lockNumbers, setLockNumbers] = useState([]);
+  const audioRef = useRef(null);
+
+  const addNextImage = useCallback(() => {
+    setTimeout(() => {
+    setLockNumbers(lockNumbers => [...lockNumbers, {lock: getRandomNumber(4000), top: getRandomNumber(window.innerHeight - 240), left: getRandomNumber(window.innerWidth - 420) }]);
+    }, 800)
+  }, []);
+
+  useEffect(() => {
+    addNextImage();
+    const audio = audioRef.current;
+    if (audio && audio.paused) {
+      audio.currentTime = 45;
+      audio.play();
+    }
+  }, [addNextImage])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{position: 'fixed', width: '100vw', height: '100vh', backgroundColor: '#000'}}>
+      <audio src={daremix} type="audio/mp3" ref={audioRef}/>
+      {lockNumbers.map((lockNumber, idx) => {
+        return <Chicken {...lockNumber} key={idx} onLoad={idx === lockNumbers.length - 1 ? addNextImage : null} />
+      })}
     </div>
   );
 }
