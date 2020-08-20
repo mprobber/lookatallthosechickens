5049110313edc14b32acc650f1059d35519836fe
Michael Probber <git@mikep.ro>
2020-08-19 19:22:46 -0700

    onbutton

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useCallback, useEffect, useRef} from 'react';
+import React, {useState, useCallback, useRef} from 'react';
 
 import daremix from './daremix.mp3';
 import './App.css';
@@ -22,18 +22,18 @@ function App() {
     }, 800)
   }, []);
 
-  useEffect(() => {
+  const letsgo = useCallback(() => {
     addNextImage();
-    const audio = audioRef.current;
+    const audio=audioRef.current;
     if (audio && audio.paused) {
       audio.currentTime = 45;
       audio.play();
     }
-  }, [addNextImage])
-
+  }, [addNextImage]);
 
   return (
     <div className="App" style={{position: 'fixed', width: '100vw', height: '100vh', backgroundColor: '#000'}}>
+      {!lockNumbers.length && <button onClick={letsgo} style={{cursor: 'pointer', width: '100vw', height: '100vh', backgroundColor: '#fff', textTransform:'uppercase'}}>are you ready?</button>}
       <audio src={daremix} type="audio/mp3" ref={audioRef}/>
       {lockNumbers.map((lockNumber, idx) => {
         return <Chicken {...lockNumber} key={idx} onLoad={idx === lockNumbers.length - 1 ? addNextImage : null} />
